1d585b9df6efb86f0ae1be72ad59e6ccd8b7e0b6
alexandre54 <alexandre.pavlovic.duval@gmail.com>
2022-12-30 21:19:15 +0100

    ProjetDataPipelineFinal

---
+++ airflow/Automatisation_python.py
@@ -0,0 +1,19 @@
+from datetime import datetime,timedelta
+from textwrap import dedent
+# Importation le fichier python projetpipeline. On importe la fonction du fichier python main_function projetpipeline.py
+from projetpipeline import main_function
+
+# importation de DAG pour instancier le DAG sur AirFlow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# Création du Dag : "ProjetDataPipelineFinal"
+# Utilisation de l'opérateur intégré PythonOperator de Airflow pour faire appel à la fonction main_function importé plus haut 
+with DAG("ProjetDataPipelineFinal",start_date=datetime(2021,1,1),schedule='*/30 * * * *',catchup=False) as dag:
+
+    task = PythonOperator(
+    task_id='task',
+    python_callable=main_function,
+)
+
+    task 
